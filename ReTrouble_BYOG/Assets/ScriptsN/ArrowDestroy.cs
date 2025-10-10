3dca1995c43773ac65eed0670946e207a4ccdf78
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-10-10 16:17:14 +0530

    Color Changing arrow

--- ReTrouble_BYOG/Assets/ScriptsN/ArrowDestroy.cs
+++ ReTrouble_BYOG/Assets/ScriptsN/ArrowDestroy.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ArrowDestroy : MonoBehaviour
 {
+    public int ColorIndex =-1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- ReTrouble_BYOG/Assets/ScriptsN/SpawnArrow_N.cs
+++ ReTrouble_BYOG/Assets/ScriptsN/SpawnArrow_N.cs
@@ -6,10 +6,13 @@ public class SpawnArrow_N : MonoBehaviour
     [SerializeField] GameObject arrowPrefab;
     public bool canSpawn = true;
     [SerializeField] float arrowSpeed=4f;
+    private int index = 0;
+    private Color[] colorArray = { Color.red ,Color.green, Color.blue};
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       gameObject.GetComponent<SpriteRenderer>().material.color = colorArray[index];
+
     }
 
     // Update is called once per frame
@@ -22,8 +25,22 @@ public class SpawnArrow_N : MonoBehaviour
         if (canSpawn && context.phase==InputActionPhase.Performed) {
             GameObject arrow=Instantiate(arrowPrefab);
             arrow.transform.localPosition = transform.localPosition;
+            arrow.GetComponentInChildren<SpriteRenderer>().material.color = colorArray[index];
+            arrow.GetComponent<ArrowDestroy>().ColorIndex = index;
             arrow.GetComponent<Rigidbody2D>().linearVelocityY = arrowSpeed;
             canSpawn = false;
+        }
+    }
+    public void ChangeColorPositive(InputAction.CallbackContext context) {
+        if (context.phase == InputActionPhase.Performed) {
+            index = (index + 1) % 3;
+            gameObject.GetComponent<SpriteRenderer>().material.color = colorArray[index];
+        }
+    }
+    public void ChangeColorNegative(InputAction.CallbackContext context) {
+        if (context.phase == InputActionPhase.Performed) {
+            index = (index - 1+3) % 3;
+            gameObject.GetComponent<SpriteRenderer>().material.color = colorArray[index];
         }
     }
 }
