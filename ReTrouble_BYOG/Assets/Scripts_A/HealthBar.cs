02d282d738e0358900d5a552a750fbae0c4ef948
Aadarsh Nayak <www.nayakaadarsh2005@gmail.com>
2025-10-12 00:07:09 +0530

    health bar updated

--- ReTrouble_BYOG/Assets/Scripts_A/HealthBar.cs
+++ ReTrouble_BYOG/Assets/Scripts_A/HealthBar.cs
@@ -1,22 +1,53 @@
-//using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
-public class HealthBar : MonoBehaviour
-{
-    [SerializeField]private HealthManager_A playerHealth;
+public class HealthBar : MonoBehaviour {
+    [SerializeField] private HealthManager_A playerHealth;
     [SerializeField] private Image totalHealthBar;
     [SerializeField] private Image currentHealthBar;
+    [SerializeField] private GameObject[] hearts;
+    [SerializeField] private Sprite[] heart;
+    private int index;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        totalHealthBar.fillAmount = playerHealth.maxHealth / 10f;
+    void Start() {
+        index = hearts.Length - 1;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        currentHealthBar.fillAmount = playerHealth.currentHealth / 10f;
+    public void descreaseHealth() {
+        if (index < 0) return;
+
+        // Change sprite
+        hearts[index].GetComponent<Image>().sprite = heart[1];
+
+        // Add jerk effect
+        StartCoroutine(HeartJerk(hearts[index].transform));
+
+        index--;
+    }
+
+    private IEnumerator HeartJerk(Transform heart) {
+        Vector3 originalScale = heart.localScale;
+        Vector3 targetScale = originalScale * 1.2f;  // slightly larger
+        float speed = 10f; // jerk speed
+        float time = 0f;
+
+        // Scale up quickly
+        while (time < 1f) {
+            time += Time.deltaTime * speed;
+            heart.localScale = Vector3.Lerp(originalScale, targetScale, time);
+            yield return null;
+        }
+
+        time = 0f;
+
+        // Scale back down
+        while (time < 1f) {
+            time += Time.deltaTime * speed;
+            heart.localScale = Vector3.Lerp(targetScale, originalScale, time);
+            yield return null;
+        }
+
+        heart.localScale = originalScale;
     }
 }
